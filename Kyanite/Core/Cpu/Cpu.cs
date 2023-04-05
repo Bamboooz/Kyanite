6ba5117a8a79691dd671cba6ce74095f64efac43
Bamboooz <bambusixmc@gmail.com>
2023-04-06 00:52:31 +0200

    Added CPU calls

---
+++ Kyanite/Core/Cpu/Cpu.cs
@@ -0,0 +1,52 @@
+namespace Kyanite.Core.Cpu
+{
+    internal static class Cpu
+    {
+        private static string CpuModel = Cosmos.Core.CPU.GetCPUBrandString();
+        private static ulong CpuUptime = Cosmos.Core.CPU.GetCPUUptime();
+        private static string CpuVendor = Cosmos.Core.CPU.GetCPUVendorName();
+        private static long CpuSpeed = Cosmos.Core.CPU.EstimateCPUSpeedFromName(Cosmos.Core.CPU.GetCPUBrandString());
+
+        private static void Update()
+        {
+            CpuModel = Cosmos.Core.CPU.GetCPUBrandString();
+            CpuUptime = Cosmos.Core.CPU.GetCPUUptime();
+            CpuSpeed = Cosmos.Core.CPU.EstimateCPUSpeedFromName(CpuModel);
+            CpuVendor = Cosmos.Core.CPU.GetCPUVendorName();
+        }
+
+        public static void Halt()
+        {
+            Cosmos.Core.CPU.Halt();
+        }
+
+        public static void Reboot()
+        {
+            Cosmos.Core.CPU.Reboot();
+        }
+
+        public static string Model()
+        {
+            Update();
+            return CpuModel;
+        }
+
+        public static long Speed()
+        {
+            Update();
+            return CpuSpeed;
+        }
+
+        public static ulong Uptime()
+        {
+            Update();
+            return CpuUptime;
+        }
+
+        public static string Vendor()
+        {
+            Update();
+            return CpuVendor;
+        }
+    }
+}
