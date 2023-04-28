f5ea4b88dcc4eef43d34333b961c316766d8aef3
Bamboooz <bambusixmc@gmail.com>
2023-04-28 09:04:08 +0200

    update LICENSE

---
+++ Kyanite/Logging/LogEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyanite.Kyanite.Logging
+{
+    internal class LogEvent
+    {
+    }
+}
